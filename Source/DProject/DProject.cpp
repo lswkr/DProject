ee53506bfb12b3cea4c7961a66585b951fd9dc1a
LSW <tmddnjs2056@naver.com>
2025-09-09 17:39:21 +0900

    Initial Project Files

---
+++ Source/DProject/DProject.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "DProject.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, DProject, "DProject" );
